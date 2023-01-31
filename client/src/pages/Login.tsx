57cbe03339045bd935b27605a3da4569d1d2c170
daryledevs <daryledevs@gmail.com>
2023-01-31 19:02:15 +0800

    Created a login page (temporary)

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login-container'>
+      <div>
+        <input placeholder='Username'/>
+        <input placeholder='Password' type="password" />
+      </div>
+    </div>
+  )
+}
+
+export default Login;
