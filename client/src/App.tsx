02c3a713c05e9ce92032cd70a22fb57a086e83b8
daryledevs <daryledevs@gmail.com>
2023-01-31 19:03:09 +0800

    Implement routing for authentication and private routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, ReactNode } from "react";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import NavigationBar from "./shared/base/NavigationBar";
+import Feed from "./pages/Feed";
+import Login from "./pages/Login";
+
+interface ProtectedProps{
+  children?: ReactNode
+}
 
 function App() {
+  const [token, setToken] = useState(true); // mock data
+
+
+  const ProtectedRoute = () => {
+    return token ? <Outlet/>  : <Navigate to='/login' replace/>;
+  };
+
+  const AuthenticationRoute = () =>{
+    return !token ? <Outlet/> : <Navigate to='/' replace />;
+  }
+
   return (
     <div className="App">
-     <h1>Hello world</h1>
+      <Routes>
+        {/* Authenticated */}
+        <Route path='/' element={<ProtectedRoute/>}>
+          <Route path="/" element={<NavigationBar />}>
+            <Route index element={<Feed />}/>
+          </Route>
+        </Route>
+
+        {/* Not yet Authenticated */}
+        <Route path='/' element={<AuthenticationRoute/>}>
+          <Route path="/login"  element={<Login />}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
