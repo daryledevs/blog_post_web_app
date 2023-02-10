b83e100df5cda1fb84be5559dd4efb7aa5d56c79
daryledevs <daryledevs@gmail.com>
2023-02-11 02:02:00 +0800

    Made the routes protected and URL inaccessible depending on the token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,61 @@
-import React, { useState, ReactNode } from "react";
-import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate, Outlet, useNavigate } from "react-router-dom";
 import NavigationBar from "./shared/base/NavigationBar";
 import Feed from "./pages/Feed";
 import Login from "./pages/Login";
-
-interface ProtectedProps{
-  children?: ReactNode
-}
+import { checkAccess } from "./redux/action/auth";
+import { useAppDispatch, useAppSelector } from "./redux/hooks/hooks";
 
 function App() {
-  const [token, setToken] = useState(true); // mock data
+  const [token, setToken] = useState(false); // mock data
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const access_status = useAppSelector((state) => state.auth.access_status);
 
+  useEffect(() => {  dispatch(checkAccess()); }, []);
+
+  useEffect(() => {
+    switch (access_status) {
+      case "Token is valid":
+      case "Login successfully":
+        setToken(true);
+        navigate("/");
+        console.log(access_status);
+        break;
+
+      case "Token is not valid":
+      case "Password is incorrect":
+      case "User not found":
+        setToken(false);
+        console.log(access_status);
+        break;
+    }
+  }, [access_status]);
 
   const ProtectedRoute = () => {
-    return token ? <Outlet/>  : <Navigate to='/login' replace/>;
+    // to prevent them to go to homepage if not authenticated yet
+    // '/' in url will not be accessible
+    return token ? (
+      <Outlet />
+    ) : (
+      <Navigate
+        to="/login"
+        replace
+      />
+    );
   };
 
   const AuthenticationRoute = () =>{
-    return !token ? <Outlet/> : <Navigate to='/' replace />;
+    // to prevent them to go to login page if already authenticated
+    // '/login' in url will not be accessible
+    return !token ? (
+      <Outlet />
+    ) : (
+      <Navigate
+        to="/"
+        replace
+      />
+    );
   }
 
   return (
