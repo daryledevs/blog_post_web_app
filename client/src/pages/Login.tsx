11f129bbde69830fa3aab34fbacf96c0e6bd905c
daryledevs <daryledevs@gmail.com>
2023-02-11 02:02:38 +0800

    Placed the login action on the form's onSubmit for logging in

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { login } from '../redux/action/auth';
+import { useAppDispatch } from '../redux/store';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useAppDispatch();
+
+  async function handleSubmit(event: React.FormEvent){
+    event.preventDefault();
+    dispatch(login({ username, password }));
+  };
+
   return (
-    <div className='login-container'>
-      <div>
-        <input placeholder='Username'/>
-        <input placeholder='Password' type="password" />
-      </div>
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username or Email"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login;
