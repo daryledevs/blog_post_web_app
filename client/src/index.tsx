a382113d02ddc31175c15fa760200fe699159ec0
daryledevs <daryledevs@gmail.com>
2023-02-10 22:55:00 +0800

    Implement redux in the web app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/*"
+            element={<App />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
