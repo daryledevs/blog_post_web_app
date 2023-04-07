93ed13b6d151638f6111211c62ee76c7f5a97f9f
daryledevs <daryledevs@gmail.com>
2023-04-08 00:42:01 +0800

    Revised the initialState schema and created addMessage action for sending message from chat reducer

--- client/src/redux/reducer/chat.ts
+++ client/src/redux/reducer/chat.ts
@@ -1,32 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 import { getChatThunk } from "../action/chat";
 import { IEChatState } from "../reduxIntface";
 
-const initialState: IEChatState = {
-  conversation_id: null as any,
-  sender_id: null as any,
-  receiver_id: null as any,
-  time_conversation: null as any,
-};
-
+const initialState: Array<Array<IEChatState>> = [
+  [
+    {
+      conversation_id: null as any,
+      first_name: null as any,
+      last_name: null as any,
+      message_id: null as any,
+      sender_id: null as any,
+      text_message: null as any,
+      time_sent: null as any,
+      user_one: null as any,
+      user_two: null as any,
+      username: null as any,
+    },
+  ],
+];
 
 const chatSlice = createSlice({
   name: "name",
   initialState,
   reducers:{
-  
+
+    addMessage: (state, action) =>{
+      let stateInstance = [...current(state)];
+
+      let findState = stateInstance.find(function(state:any, index:any){
+        if (state[0].conversation_id === action.payload.conversation_id) return state;
+      });
+      
+      let index = stateInstance.findIndex(
+        (state: any, index: any) =>
+          state[0].conversation_id === findState?.[0].conversation_id
+      );
+
+      let stateContent = stateInstance[index];
+      stateInstance[index] = [...stateContent, { ...action.payload }];
+
+      return [...stateInstance];
+    }
   },
+
   extraReducers(builder) {
     builder.addCase(getChatThunk.rejected, (state, action) => {
-      console.log("CHAT: ", action?.payload?.message);
+      console.log("CHAT REJECT: ", action?.payload?.message);
     });
 
     builder.addCase(getChatThunk.fulfilled, (state, action) => {
-      console.log("CHAT: ", action.payload);
-      return { ...action.payload };
+      return [...action.payload]
     });
   },
 });
 
-export const {  } = chatSlice.actions;
+export const { addMessage } = chatSlice.actions;
 export default chatSlice.reducer;
