f419a68ad27eed537060e526d6d5eb219cf8ea49
daryledevs <daryledevs@gmail.com>
2023-04-08 23:28:07 +0800

    Implement socket io and establish a connection from the server, allowing it to receive messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Routes, Route, Navigate, Outlet, useNavigate } from "react-router-dom";
-import NavigationBar from "./shared/base/NavigationBar";
+import Index from "../src/pages/Index";
 import Feed from "./pages/Feed";
 import Login from "./pages/Login";
 import { checkAccess } from "./redux/action/auth";
 import { useAppDispatch, useAppSelector } from "./redux/hooks/hooks";
+import Profile from "./pages/Profile";
+import Message from "./pages/Message";
+import { useDispatch } from "react-redux";
+import { io } from "socket.io-client";
+import { getMessage } from "./redux/reducer/chat";
 
 function App() {
   const [token, setToken] = useState(false); // mock data
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+  const appDispatch = useAppDispatch();
+  const dispatch = useDispatch();
+  const socket = useRef(io("ws://localhost:8900"));
   const access_status = useAppSelector((state) => state.auth.access_status);
 
-  useEffect(() => {  dispatch(checkAccess()); }, []);
+  const userData = useAppSelector((state) => state.user);
+  const [comingMessage, setComingMessage] = useState<any>();
+
+  // socket handler
+  useEffect(() => {
+    if (userData.user_id) {
+      socket.current.emit("addUser", userData.user_id);
+      socket.current.on("getMessage", (data: any) => {
+        setComingMessage({
+          conversation_id: data.conversation_id,
+          sender_id: data.senderId,
+          text_message: data.text,
+        });
+      });
+    }
+  }, [socket, userData]);
+
+  useEffect(() => {
+    if (comingMessage) dispatch(getMessage(comingMessage));
+  }, [comingMessage]);
+
+  useEffect(() => {
+    appDispatch(checkAccess());
+  }, [access_status]);
 
   useEffect(() => {
     switch (access_status) {
@@ -45,7 +75,7 @@ function App() {
     );
   };
 
-  const AuthenticationRoute = () =>{
+  const AuthenticationRoute = () => {
     // to prevent them to go to login page if already authenticated
     // '/login' in url will not be accessible
     return !token ? (
@@ -56,21 +86,44 @@ function App() {
         replace
       />
     );
-  }
+  };
 
   return (
     <div className="App">
       <Routes>
         {/* Authenticated */}
-        <Route path='/' element={<ProtectedRoute/>}>
-          <Route path="/" element={<NavigationBar />}>
-            <Route index element={<Feed />}/>
+        <Route
+          path="/"
+          element={<ProtectedRoute />}
+        >
+          <Route
+            path="/"
+            element={<Index />}
+          >
+            <Route
+              index
+              element={<Feed />}
+            />
+            <Route
+              path="/profile"
+              element={<Profile />}
+            />
+            <Route
+              path="/message"
+              element={<Message />}
+            />
           </Route>
         </Route>
 
         {/* Not yet Authenticated */}
-        <Route path='/' element={<AuthenticationRoute/>}>
-          <Route path="/login"  element={<Login />}/>
+        <Route
+          path="/"
+          element={<AuthenticationRoute />}
+        >
+          <Route
+            path="/login"
+            element={<Login />}
+          />
         </Route>
       </Routes>
     </div>
