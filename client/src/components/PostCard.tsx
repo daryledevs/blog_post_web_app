7b66341dfbad356d9450bdbdbc4b95dc528245ee
daryledevs <daryledevs@gmail.com>
2023-04-25 07:33:10 +0800

    Fetch data when user reach the bottom by scrolling down from the homepage, and initial UI changes for PostCard component

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -9,17 +9,25 @@ import emojiIcon from "../assets/icons/emoji-icon.png";
 import shareIcon from "../assets/icons/share.png";
 // mock post
 import user_post from "../assets/images/mock_post.jpg";
+import { useAppSelector } from '../redux/hooks/hooks';
 
-function PostCard() {
+interface IEPostCard{
+  postData:any;
+}
+
+function PostCard({ postData }: IEPostCard) {
+  const follow = useAppSelector(state => state.follow);
+  const user = follow.following.find(item => item.user_id === postData.followed_id);
+
   return (
     <div className="post-card-container">
       <div className='post-card__header'>
         <img src={avatar} className="post-card__header-avatar" alt='user-profile-picture'/>
-        <p>Daryle De Vera</p>
+        <p>{user?.username}</p>
         <img src={settingIcon} className='post-card__header-settings'/>
       </div>
       <div className='post-card__body'>
-        <img src={user_post} className="post-card__body-post" />
+        <img src={postData.image_url} className="post-card__body-post" />
         <div className='post-card__body-icons-container'>
           <img src={heartIcon_uncheck} alt='like post' />
           <img src={commentIcon} alt='comment section' />
@@ -29,8 +37,8 @@ function PostCard() {
       </div>
       <div className='post-card__details'>
         <div className='post-card__details-caption'>
-          <h4>Daryle De Vera</h4>
-          <p>Sunsets never disappoint us - they're always beautiful.</p>
+          <h4>{user?.username}</h4>
+          <p>{postData.caption}</p>
         </div>
         <div className='post-card__details-meta'>
           <p>View all 20 comments</p>
--- client/src/pages/Feed.tsx
+++ client/src/pages/Feed.tsx
@@ -1,12 +1,90 @@
-import React from 'react'
-import PostCard from '../components/PostCard';
+import React, { useState, useEffect, useRef } from "react";
+import PostCard from "../components/PostCard";
+import { useAppSelector } from "../redux/hooks/hooks";
+import api from "../assets/data/api";
 
 function Feed() {
+  const user = useAppSelector((state) => state.user);
+  const token: any = sessionStorage.getItem("token");
+  const header = { headers: { Authorization: `Bearer ${token}` } };
+  const feedRef = useRef<HTMLDivElement>(null);
+  const [userFeed, setUserFeed] = useState<any>({ feeds: [] });
+  const [addFeedTrigger, setAddFeedTrigger] = useState<boolean>(false);
+  const [isFirstLoad, setIsFirstLoad] = useState<boolean>(userFeed.feeds.length === 0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [totalFeed, setTotalFeed] = useState<any>(0);
+  const [noPostTrigger, setNoPostTrigger] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getIds = userFeed.feeds.map((feed: any) => {
+      return feed.post_id;
+    });
+
+    const getUserFeed = async () => {
+      try {
+        const response = await api.post(
+          "users/feed",
+          { post_id_arr: getIds },
+          header
+        );
+        return setUserFeed({ feeds: [...userFeed.feeds, ...response.data.feed] });
+      } catch (error) {
+        console.log(error);
+        return;
+      } finally {
+        setIsLoading(false);
+        setIsFirstLoad(false);
+      }
+    };
+
+    getUserFeed();
+  }, [addFeedTrigger]);
+
+  useEffect(() => {
+    const win: Window = window;
+    const isBottom = (element: any) => { return element.getBoundingClientRect().bottom <= window.innerHeight; };
+
+    const promptState = () => {
+      setAddFeedTrigger(true);
+      setIsLoading(true);
+    };
+
+    const getTotalFeed = async () => {
+      try {
+        const response = await api.get("users/feed/count", header);
+        return setTotalFeed(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const onScroll: EventListener = (event: Event) => {
+      const feedLength = userFeed.feeds.length;
+      if (isBottom(feedRef.current)) {
+        if (totalFeed <= feedLength) {
+          getTotalFeed();
+          if (totalFeed <= feedLength) return setNoPostTrigger(true);
+          promptState();
+        } else {
+          promptState();
+        }
+      }
+    };
+
+    win?.addEventListener("scroll", onScroll);
+    return () => win?.removeEventListener("scroll", onScroll);
+  }, []);
+
+  if (isFirstLoad) return <></>;
+
   return (
-    <div className='feed-container'>
-      <PostCard/>
+    <div ref={feedRef} className="feed-container">
+      {userFeed.feeds?.map((feed: any) => {
+        return <PostCard postData={feed} />;
+      })}
+      {noPostTrigger && <>No Post To Show</>}
     </div>
-  )
+  );
 }
 
 export default Feed;
