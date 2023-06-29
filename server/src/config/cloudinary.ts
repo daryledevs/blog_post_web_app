9333deacd116a923f0b90fd22cac5028dd1315b7
daryledevs <daryledevs@gmail.com>
2023-06-30 03:32:35 +0800

    Created a config file for cloudinary

---
+++ server/src/config/cloudinary.ts
@@ -0,0 +1,24 @@
+import cloudinary from "cloudinary";
+import * as fs from "fs";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+cloudinary.v2.config({
+  cloud_name: process.env.STORAGE_NAME,
+  api_key: process.env.STORAGE_API_KEY,
+  api_secret: process.env.STORAGE_API_SECRET,
+  secure: true,
+});
+
+async function uploadAndDeleteLocal(path: any) {
+  const result = await cloudinary.v2.uploader.upload(path, {
+    UNIQUE_FILENAME: true,
+  });
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+    console.log("Delete File successfully.");
+  });
+  return { image_id: result.public_id, image_url: result.url };
+}
+
+export default uploadAndDeleteLocal;
