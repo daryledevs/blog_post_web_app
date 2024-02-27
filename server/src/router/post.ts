78f9bc179d0b6540ac434d249d7b9b711be32aa4
darylescode <daryledevs@gmail.com>
2024-02-27 14:52:41 +0800

    Update the URI structure for posts' likes and getting user's post

--- server/src/router/post.ts
+++ server/src/router/post.ts
@@ -14,13 +14,13 @@ const router = express.Router();
 const uploadOption = uploadImage("./uploads/post");
 const option_field = [ { name: "img", maxCount: 1 }, { name: "imgs", maxCount: 7 } ];
 
-router.get("/:user_id", getUserPost);
+router.get("/users/:user_id", getUserPost);
 router.patch("/:post_id", editPost);
 router.delete("/:post_id", deletePost);
 
 router.get("/:post_id/likes", getLikesCountForPost);
-router.get("/:post_id/likes/:user_id", checkUserLikeStatusForPost);
-router.put("/:post_id/likes/:user_id", toggleUserLikeForPost);
+router.get("/:post_id/likes/:user_id/likes", checkUserLikeStatusForPost);
+router.put("/:post_id/likes/:user_id/likes", toggleUserLikeForPost);
 router.post("/", uploadOption.fields(option_field), newPost);
 
 export default router;
