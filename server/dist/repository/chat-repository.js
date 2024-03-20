f4fa43c48dc4cbcfae7ef51c1d2053644364f10f
darylescode <daryledevs@gmail.com>
2024-03-20 09:03:44 +0800

    Create chat repository for executing SQL queries with kysely

---
+++ server/dist/repository/chat-repository.js
@@ -0,0 +1,147 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("../database/database");
+const database_2 = __importDefault(require("../exception/database"));
+class ChatRepository {
+    static getUserConversationHistoryByUserId(user_id, conversations) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return yield database_1.db
+                    .selectFrom("conversations")
+                    .select([
+                    "conversations.conversation_id",
+                    "conversations.user_one_id",
+                    "conversations.user_two_id",
+                ])
+                    .leftJoin((eb) => eb
+                    .selectFrom("users")
+                    .select([
+                    "users.user_id",
+                    "users.username",
+                    "users.first_name",
+                    "users.last_name",
+                    "users.avatar_url",
+                ])
+                    .as("users"), (join) => join.onRef("users.user_id", "=", (eb) => eb
+                    .case()
+                    .when("conversations.user_one_id", "=", user_id)
+                    .then(eb.ref("conversations.user_two_id"))
+                    .else(eb.ref("conversations.user_one_id"))
+                    .end()))
+                    .selectAll("users")
+                    .where("conversations.conversation_id", "not in", conversations)
+                    .execute();
+            }
+            catch (error) {
+                throw database_2.default.fromError(error);
+            }
+        });
+    }
+    ;
+    static getHistoryByConversationId(conversation_id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return yield database_1.db
+                    .selectFrom("conversations")
+                    .selectAll()
+                    .where("conversation_id", "=", conversation_id)
+                    .executeTakeFirst();
+            }
+            catch (error) {
+                throw database_2.default.fromError(error);
+            }
+        });
+    }
+    ;
+    static findConversationByUserId(user_id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return yield database_1.db
+                    .selectFrom("conversations")
+                    .select([
+                    "conversations.conversation_id",
+                    "conversations.user_one_id",
+                    "conversations.user_two_id",
+                ])
+                    .where((eb) => eb.or([
+                    eb("conversations.user_one_id", "=", user_id[0])
+                        .and("conversations.user_two_id", "=", user_id[1]),
+                    eb("conversations.user_one_id", "=", user_id[2])
+                        .and("conversations.user_two_id", "=", user_id[3]),
+                ]))
+                    .executeTakeFirst();
+            }
+            catch (error) {
+                throw database_2.default.fromError(error);
+            }
+            ;
+        });
+    }
+    ;
+    static getMessagesByConversationId(conversation_id, ids) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return yield database_1.db
+                    .selectFrom("messages")
+                    .selectAll()
+                    .where((eb) => eb.and([
+                    eb("conversation_id", "=", conversation_id),
+                    eb("message_id", "not in", ids),
+                ]))
+                    .limit(10)
+                    .execute();
+            }
+            catch (error) {
+                throw database_2.default.fromError(error);
+            }
+            ;
+        });
+    }
+    ;
+    static saveNewConversation(conversation) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const { insertId } = yield database_1.db
+                    .insertInto("conversations")
+                    .values(conversation)
+                    .executeTakeFirst();
+                return insertId;
+            }
+            catch (error) {
+                throw database_2.default.fromError(error);
+            }
+            ;
+        });
+    }
+    ;
+    static saveNewMessage(message) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield database_1.db
+                    .insertInto("messages")
+                    .values(message)
+                    .executeTakeFirst();
+                return "New message created";
+            }
+            catch (error) {
+                throw database_2.default.fromError(error);
+            }
+            ;
+        });
+    }
+    ;
+}
+;
+exports.default = ChatRepository;
---
+++ server/src/repository/chat-repository.ts
@@ -0,0 +1,124 @@
+import { db }                            from "../database/database";
+import DatabaseException                 from "../exception/database";
+import { Message, NewMessage }           from "../types/messages";
+import { Conversation, NewConversation } from "../types/conversations";
+
+class ChatRepository {
+  static async getUserConversationHistoryByUserId(user_id: number, conversations: number[]): Promise<any> {
+    try {
+      return await db
+        .selectFrom("conversations")
+        .select([
+          "conversations.conversation_id",
+          "conversations.user_one_id",
+          "conversations.user_two_id",
+        ])
+        .leftJoin(
+          (eb) =>
+            eb
+              .selectFrom("users")
+              .select([
+                "users.user_id",
+                "users.username",
+                "users.first_name",
+                "users.last_name",
+                "users.avatar_url",
+              ])
+              .as("users"),
+          (join) =>
+            join.onRef("users.user_id", "=", (eb) =>
+              eb
+                .case()
+                .when("conversations.user_one_id", "=", user_id)
+                .then(eb.ref("conversations.user_two_id"))
+                .else(eb.ref("conversations.user_one_id"))
+                .end()
+            )
+        )
+        .selectAll("users")
+        .where("conversations.conversation_id", "not in", conversations)
+        .execute();
+    } catch (error) {
+      throw DatabaseException.fromError(error);
+    }
+  };
+
+  static async getHistoryByConversationId(conversation_id: number): Promise<Conversation | undefined> {
+    try {
+      return await db
+        .selectFrom("conversations")
+        .selectAll()
+        .where("conversation_id", "=", conversation_id)
+        .executeTakeFirst();
+    } catch (error) {
+      throw DatabaseException.fromError(error);
+    }
+  };
+
+  static async findConversationByUserId(user_id: number[]): Promise<Conversation | undefined> {
+    try {
+      return await db
+        .selectFrom("conversations")
+        .select([
+          "conversations.conversation_id",
+          "conversations.user_one_id",
+          "conversations.user_two_id",
+        ])
+        .where((eb) =>
+          eb.or([
+            eb("conversations.user_one_id", "=", user_id[0])
+            .and("conversations.user_two_id", "=", user_id[1]),
+            eb("conversations.user_one_id", "=", user_id[2])
+            .and("conversations.user_two_id", "=", user_id[3]),
+          ])
+        )
+        .executeTakeFirst();
+    } catch (error) {
+      throw DatabaseException.fromError(error);
+    };
+  };
+
+  static async getMessagesByConversationId(conversation_id: number, ids: number[] | number): Promise<Message[]> {
+    try {
+      return await db
+        .selectFrom("messages")
+        .selectAll()
+        .where((eb) => eb.and([
+          eb("conversation_id", "=", conversation_id),
+          eb("message_id", "not in", ids),
+        ]))
+        .limit(10)
+        .execute();
+    } catch (error) {
+      throw DatabaseException.fromError(error);
+    };
+  };
+
+  static async saveNewConversation(conversation: NewConversation): Promise<bigint | undefined> {
+    try {
+      const { insertId } = await db
+        .insertInto("conversations")
+        .values(conversation)
+        .executeTakeFirst();
+
+      return insertId;
+    } catch (error) {
+      throw DatabaseException.fromError(error);
+    };
+  };
+
+  static async saveNewMessage(message: NewMessage): Promise<string> {
+    try {
+      await db
+        .insertInto("messages")
+        .values(message)
+        .executeTakeFirst();
+
+      return "New message created";
+    } catch (error) {
+      throw DatabaseException.fromError(error);
+    };
+  };
+};
+
+export default ChatRepository;
