bf6c1b91856aa8651ddc612bfa797fa75a7ea7e2
darylescode <darylescode@gmail.com>
2024-05-08 22:30:34 +0800

    Add user data retrieval and update clickedLink state in Index.tsx

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,9 +1,10 @@
-import React, { useState }     from "react";
+import React, { useEffect, useState }     from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 import CreatePost              from "@/shared/modals/CreatePost";
 import Sidebar                 from "@/components/sidebar/Sidebar";
 import SearchBar               from "@/components/search-bar/SearchBar";
+import { useGetUserDataQuery } from "@/redux/api/userApi";
 
 export type ClickedLink = {
   previous: string;
@@ -13,10 +14,22 @@ export type ClickedLink = {
 function Index() {
   const { hash, pathname, search } = useLocation();
   
+  const userDataApi = useGetUserDataQuery({ person: "" });
+
   const [clickedLink, setClickedLink] = useState<ClickedLink>({
     previous: "",
     current: pathname,
   });
+
+  useEffect(() => {
+    if (userDataApi.data) {
+      const user = userDataApi.data.user;
+      const path = `/${user.username}` === pathname ? "/profile" : pathname;
+      setClickedLink({ previous: "", current: path });
+    }
+  }, [userDataApi?.data?.user]);
+
+  if (userDataApi.isLoading || !userDataApi.data) return null;
 
   return (
     <div className="index__container">
