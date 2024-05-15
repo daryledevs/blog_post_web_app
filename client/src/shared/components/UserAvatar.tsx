f2a8c1fe415eab2c7effb681228cad8dba3f1e69
darylescode <daryledevs@gmail.com>
2024-05-15 19:37:47 +0800

    Create reusable UserAvatar component for displaying user avatars

---
+++ client/src/shared/components/UserAvatar.tsx
@@ -0,0 +1,18 @@
+import usersPicture from "@/assets/icons/avatar.png";
+
+type UserAvatarProps = {
+  avatar_url: string;
+  className: string;
+};
+
+function UserAvatar({ avatar_url, className } : UserAvatarProps) {
+  return (
+    <img
+      alt=""
+      className={className}
+      src={avatar_url ? avatar_url : usersPicture}
+    />
+  );
+};
+
+export default UserAvatar;
