8266dc032e763e52f4adc0e95b57b1b6e3723a04
darylescode <daryledevs@gmail.com>
2024-05-23 05:23:03 +0800

    Refactor ChatBoxMessageList component to show a loading placeholder

--- client/src/components/message/chatbox/ChatBoxMessageList.tsx
+++ client/src/components/message/chatbox/ChatBoxMessageList.tsx
@@ -34,21 +34,23 @@ function ChatBoxMessageList({
       />
       {comingMessage.length ? (
         <React.Fragment>
+          {comingMessage?.map((data: any, index: number) => (
+            <ChatBoxMessage
+              key={data.message_id}
+              observerRef={observerRef}
+              text_message={data?.text_message}
+              value={classNameChecker(data.sender_id)}
+            />
+          ))}
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
           {isLoading ? (
             <div style={{ width: "100%", textAlign: "center" }}>Loading...</div>
           ) : (
-            comingMessage?.map((data: any, index: number) => (
-              <ChatBoxMessage
-                key={data.message_id}
-                observerRef={observerRef}
-                text_message={data?.text_message}
-                value={classNameChecker(data.sender_id)}
-              />
-            ))
+            <ChatBoxStartConversation username={username} />
           )}
         </React.Fragment>
-      ) : (
-        <ChatBoxStartConversation username={username} />
       )}
       <div ref={chatListRef} />
     </div>
