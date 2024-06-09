47e81c05d51f668ff6539ce4b121849930134dfb
darylescode <daryledevs@gmail.com>
2024-06-09 19:05:57 +0800

    Create User DTO class for request and response data safety using class-transformer and class-validator libraries

---
+++ server/dist/dto/user.dto.js
@@ -0,0 +1,154 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const class_validator_1 = require("class-validator");
+const class_transformer_1 = require("class-transformer");
+class UserDto {
+    id;
+    uuid;
+    username;
+    email;
+    password;
+    roles;
+    first_name;
+    last_name;
+    age;
+    avatar_url;
+    birthday;
+    created_at;
+    constructor(user) {
+        Object.assign(this, user);
+    }
+    // Getters
+    getId() {
+        return this.id;
+    }
+    getUuid() {
+        return this.uuid;
+    }
+    getUsername() {
+        return this.username;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRoles() {
+        return this.roles;
+    }
+    getPassword() {
+        return this.password;
+    }
+    getFirstName() {
+        return this.first_name;
+    }
+    getLastName() {
+        return this.last_name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getAvatar() {
+        return this.avatar_url;
+    }
+    getBirthday() {
+        return this.birthday;
+    }
+    getCreatedAt() {
+        return this.created_at;
+    }
+    // Setters
+    setId(value) {
+        this.id = value;
+    }
+    setUuid(value) {
+        this.uuid = value;
+    }
+    setUsername(value) {
+        this.username = value;
+    }
+    setEmail(value) {
+        this.email = value;
+    }
+    setPassword(value) {
+        this.password = value;
+    }
+    setRoles(value) {
+        this.roles = value;
+    }
+    setFirstName(value) {
+        this.first_name = value;
+    }
+    setLastName(value) {
+        this.last_name = value;
+    }
+    setAge(value) {
+        this.age = value;
+    }
+    setAvatar(value) {
+        this.avatar_url = value;
+    }
+    setBirthday(value) {
+        this.birthday = value;
+    }
+    setCreatedAt(value) {
+        this.created_at = value;
+    }
+}
+__decorate([
+    (0, class_transformer_1.Exclude)({ toPlainOnly: true })
+], UserDto.prototype, "id", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)()
+], UserDto.prototype, "uuid", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.Length)(5, 30, { message: "Username must be between 5 and 30 characters" })
+], UserDto.prototype, "username", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsEmail)({}, { message: "Invalid email" })
+], UserDto.prototype, "email", void 0);
+__decorate([
+    (0, class_transformer_1.Exclude)({ toPlainOnly: true }),
+    (0, class_validator_1.Length)(6, 100, { message: "Password must be at least 6 characters" })
+], UserDto.prototype, "password", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsString)({ message: "Roles must be a string" }),
+    (0, class_validator_1.IsIn)(["user", "admin"], { message: "Invalid role" })
+], UserDto.prototype, "roles", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsString)({ message: "First name must be a string" })
+], UserDto.prototype, "first_name", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsString)({ message: "Last name must be a string" })
+], UserDto.prototype, "last_name", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsInt)({ message: "Age must be a number" })
+], UserDto.prototype, "age", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsString)({ message: "Avatar url must be a string" })
+], UserDto.prototype, "avatar_url", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsString)({ message: "Birthday must be a string" })
+], UserDto.prototype, "birthday", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.IsOptional)()
+], UserDto.prototype, "created_at", void 0);
+exports.default = UserDto;
---
+++ server/src/dto/user.dto.ts
@@ -0,0 +1,168 @@
+import {
+  IsInt,
+  Length,
+  IsEmail,
+  IsString,
+  IsOptional,
+  IsIn,
+}                          from "class-validator";
+import { SelectUsers }     from "@/types/table.types";
+import { Exclude, Expose } from "class-transformer";
+
+class UserDto {
+  @Exclude({ toPlainOnly: true })
+  private id!: number;
+
+  @Expose()
+  private uuid!: string;
+
+  @Expose()
+  @Length(5, 30, { message: "Username must be between 5 and 30 characters" })
+  private username!: string;
+
+  @Expose()
+  @IsEmail({}, { message: "Invalid email" })
+  private email!: string;
+
+  @Exclude({ toPlainOnly: true })
+  @Length(6, 100, { message: "Password must be at least 6 characters" })
+  private password!: string;
+
+  @Expose()
+  @IsString({ message: "Roles must be a string" })
+  @IsIn(["user", "admin"], { message: "Invalid role" })
+  private roles: string | null | undefined;
+
+  @Expose()
+  @IsOptional()
+  @IsString({ message: "First name must be a string" })
+  private first_name: string | null | undefined;
+
+  @Expose()
+  @IsOptional()
+  @IsString({ message: "Last name must be a string" })
+  private last_name: string | null | undefined;
+
+  @Expose()
+  @IsOptional()
+  @IsInt({ message: "Age must be a number" })
+  private age: number | null | undefined;
+
+  @Expose()
+  @IsOptional()
+  @IsString({ message: "Avatar url must be a string" })
+  private avatar_url: string | null | undefined;
+
+  @Expose()
+  @IsOptional()
+  @IsString({ message: "Birthday must be a string" })
+  private birthday: string | null | undefined;
+
+  @Expose()
+  @IsOptional()
+  private created_at: Date | null | undefined;
+
+  constructor(user: Partial<SelectUsers>) {
+    Object.assign(this, user);
+  }
+
+  // Getters
+  getId(): number {
+    return this.id;
+  }
+  
+  getUuid(): string {
+    return this.uuid;
+  }
+  
+  getUsername(): string {
+    return this.username;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+  getRoles(): string | null | undefined {
+    return this.roles;
+  }
+
+  getPassword(): string {
+    return this.password;
+  }
+
+  getFirstName(): string | null | undefined {
+    return this.first_name;
+  }
+
+  getLastName(): string | null | undefined {
+    return this.last_name;
+  }
+
+  getAge(): number | null | undefined {
+    return this.age;
+  }
+
+  getAvatar(): string | null | undefined {
+    return this.avatar_url;
+  }
+
+  getBirthday(): string | null | undefined {
+    return this.birthday;
+  }
+
+  getCreatedAt(): Date | null | undefined {
+    return this.created_at;
+  }
+
+  // Setters
+  setId(value: number) {
+    this.id = value;
+  }
+
+  setUuid(value: string) {
+    this.uuid = value;
+  }
+  
+  setUsername(value: string) {
+    this.username = value;
+  }
+
+  setEmail(value: string) {
+    this.email = value;
+  }
+
+  setPassword(value: string) {
+    this.password = value;
+  }
+
+  setRoles(value: string | null) {
+    this.roles = value;
+  }
+
+  setFirstName(value: string | null) {
+    this.first_name = value;
+  }
+
+  setLastName(value: string | null) {
+    this.last_name = value;
+  }
+
+  setAge(value: number | null) {
+    this.age = value;
+  }
+
+  setAvatar(value: string | null) {
+    this.avatar_url = value;
+  }
+
+  setBirthday(value: string | null) {
+    this.birthday = value;
+  }
+
+  setCreatedAt(value: Date | null) {
+    this.created_at = value;
+  }
+}
+
+export default UserDto;
