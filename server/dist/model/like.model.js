d657c7b880d865042c9457536cc351fa21b6065b
darylescode <daryledevs@gmail.com>
2024-07-01 04:37:12 +0800

    Refactor Like model class to include post_uuid and user_uuid properties

--- server/dist/model/like.model.js
+++ server/dist/model/like.model.js
@@ -4,13 +4,17 @@ class Like {
     id;
     uuid;
     post_id;
+    post_uuid;
     user_id;
+    user_uuid;
     created_at;
-    constructor(id, uuid, post_id, user_id, created_at) {
+    constructor(id, uuid, post_id, post_uuid, user_id, user_uuid, created_at) {
         this.id = id;
         this.uuid = uuid;
         this.post_id = post_id;
+        this.post_uuid = post_uuid;
         this.user_id = user_id;
+        this.user_uuid = user_uuid;
         this.created_at = created_at;
     }
     // Getters
@@ -23,8 +27,14 @@ class Like {
     getPostId() {
         return this.post_id;
     }
+    getPostUuid() {
+        return this.post_uuid;
+    }
     getUserId() {
         return this.user_id;
+    }
+    getUserUuid() {
+        return this.user_uuid;
     }
     getCreatedAt() {
         return this.created_at;
@@ -39,8 +49,14 @@ class Like {
     setPostId(post_id) {
         this.post_id = post_id;
     }
+    setPostUuid(post_uuid) {
+        this.post_uuid = post_uuid;
+    }
     setUserId(user_id) {
         this.user_id = user_id;
+    }
+    setUserUuid(user_uuid) {
+        this.user_uuid = user_uuid;
     }
     setCreatedAt(created_at) {
         this.created_at = created_at;
--- server/src/model/like.model.ts
+++ server/src/model/like.model.ts
@@ -1,15 +1,27 @@
 class Like {
-  private id: number; 
+  private id: number;
   private uuid: any;
   private post_id: number;
+  private post_uuid: any;
   private user_id: number;
+  private user_uuid: any;
   private created_at: Date | null;
 
-  constructor(id: number, uuid: any, post_id: number, user_id: number, created_at: Date | null) {
+  constructor(
+    id: number,
+    uuid: any,
+    post_id: number,
+    post_uuid: any,
+    user_id: number,
+    user_uuid: any, 
+    created_at: Date | null
+  ) {
     this.id = id;
     this.uuid = uuid;
     this.post_id = post_id;
+    this.post_uuid = post_uuid;
     this.user_id = user_id;
+    this.user_uuid = user_uuid;
     this.created_at = created_at;
   }
 
@@ -26,8 +38,16 @@ class Like {
     return this.post_id;
   }
 
+  public getPostUuid(): any {
+    return this.post_uuid;
+  }
+
   public getUserId(): number {
     return this.user_id;
+  }
+
+  public getUserUuid(): any {
+    return this.user_uuid;
   }
 
   public getCreatedAt(): Date | null {
@@ -47,8 +67,16 @@ class Like {
     this.post_id = post_id;
   }
 
+  public setPostUuid(post_uuid: any): void {
+    this.post_uuid = post_uuid;
+  }
+
   public setUserId(user_id: number): void {
     this.user_id = user_id;
+  }
+
+  public setUserUuid(user_uuid: any): void {
+    this.user_uuid = user_uuid;
   }
 
   public setCreatedAt(created_at: Date | null): void {
