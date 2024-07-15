d84eb1c626f9548e44765b148c0862e41e68240b
darylescode <daryledevs@gmail.com>
2024-07-16 05:40:35 +0800

    Update postApi endpoints to reflect server changes

--- client/src/redux/api/postApi.ts
+++ client/src/redux/api/postApi.ts
@@ -2,15 +2,17 @@ import baseApi from "./baseApi";
 
 const postApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
+    // get user post
     getUserPost: build.query<any, { user_id: number }>({
       query: ({ user_id }) => ({
-        url: `/posts?user_id=${user_id}`,
+        url: `/posts/by-user/${user_id}`,
         method: "GET",
       }),
     }),
+    // get user total posts
     getUserTotalPosts: build.query<any, { user_id: number }>({
       query: ({ user_id }) => ({
-        url: `/posts/stats?user_id=${user_id}`,
+        url: `/posts/by-user/${user_id}/stats`,
         method: "GET",
       }),
     }),
