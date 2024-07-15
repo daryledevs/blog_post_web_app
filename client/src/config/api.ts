5f27359c1272659b718656ba65e80ea3bc242668
darylescode <daryledevs@gmail.com>
2024-07-16 03:33:31 +0800

    Resolve type error with Axios request headers in interceptor

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -27,11 +27,8 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = sessionStorage.getItem("token");
-    if (token) {
-      config.headers = {
-        ...config.headers,
-        Authorization: `Bearer ${token}`,
-      };
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
