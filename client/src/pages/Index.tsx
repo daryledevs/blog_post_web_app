5240183fe11bc7b6e204d740b796bca35c6b413c
darylescode <daryledevs@gmail.com>
2024-07-22 17:58:38 +0800

    Refactor layout and styling for sidebar and searchbar components

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -3,17 +3,28 @@ import CreatePost               from "@/shared/modals/CreatePost";
 import Sidebar                  from "@/components/sidebar/Sidebar";
 import SearchBar                from "@/components/search-bar/SearchBar";
 import useUpdateNavigationState from "@/hooks/useUpdateNavigatePage";
+import { selectSidebar } from "@/redux/slices/sidebarSlice";
+import { useSelector } from "react-redux";
 
 function Index() {
   const { isLoading, error } = useUpdateNavigationState();
+  const sidebarState = useSelector(selectSidebar);
   if (isLoading) return null;
 
   return (
     <div className="index__container">
       <CreatePost />
       <div className="index__sidebar">
-        <Sidebar />
-        <SearchBar />
+        <div
+          className={
+            sidebarState.current === "Search"
+              ? "index__sidebar-search-bar-active"
+              : "index__sidebar-container"
+          }
+        >
+          <Sidebar />
+          <SearchBar />
+        </div>
       </div>
       <Outlet />
     </div>
