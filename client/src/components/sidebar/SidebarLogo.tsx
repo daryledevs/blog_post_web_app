848e9845f7356b982223e47c7cac8ab6c11f6e36
darylescode <daryledevs@gmail.com>
2024-07-22 15:55:05 +0800

    Update sidebar width to 25% for better visibility and replace app text logo into icon when search tab is active

--- client/src/components/sidebar/SidebarLogo.tsx
+++ client/src/components/sidebar/SidebarLogo.tsx
@@ -1,11 +1,22 @@
-import InstagramText from "@/assets/images/instagram-logo.svg?react";
-import InstagramLogo from "@/assets/icons/svg/instagram-logo.svg?react";
+import InstagramText     from "@/assets/images/instagram-logo.svg?react";
+import InstagramLogo     from "@/assets/icons/svg/instagram-logo.svg?react";
+import { useSelector }   from "react-redux";
+import { selectSidebar } from "@/redux/slices/sidebarSlice";
 
 function SidebarLogo() {
+  const sidebarState = useSelector(selectSidebar);
+  const current = sidebarState.current;
+
   return (
     <div className="sidebar-logo">
-      <InstagramText className="sidebar-logo__text" />
       <InstagramLogo className="sidebar-logo__icon" />
+      {current === "Search" ? (
+        <InstagramLogo 
+          className="sidebar-logo__icon sidebar-logo__icon-search-active" 
+        />
+      ) : (
+        <InstagramText className="sidebar-logo__text" />
+      )}
     </div>
   );
 }
