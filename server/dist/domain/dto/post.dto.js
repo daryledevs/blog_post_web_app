39f5306557406e8d7c5b33534d9e2799a2b5e639
darylescode <daryledevs@gmail.com>
2024-08-17 06:38:20 +0800

    Enhance PostDto: Add files and user_uuid properties, with validation

--- server/dist/domain/dto/post.dto.js
+++ server/dist/domain/dto/post.dto.js
@@ -16,16 +16,20 @@ class PostDto {
     uuid;
     image_id;
     image_url;
+    files;
     user_id;
+    user_uuid;
     caption;
     privacy_level;
     created_at;
-    constructor(id, uuid, image_id, image_url, user_id, caption, privacy_level, created_at) {
+    constructor(id, uuid, image_id, image_url, files, user_id, user_uuid, caption, privacy_level, created_at) {
         this.id = id;
         this.uuid = uuid;
         this.image_id = image_id;
         this.image_url = image_url;
+        this.files = files;
         this.user_id = user_id;
+        this.user_uuid = user_uuid;
         this.caption = caption;
         this.privacy_level = privacy_level;
         this.created_at = created_at;
@@ -43,8 +47,14 @@ class PostDto {
     getImageUrl() {
         return this.image_url;
     }
+    getFiles() {
+        return this.files;
+    }
     getUserId() {
         return this.user_id;
+    }
+    getUserUuid() {
+        return this.user_uuid;
     }
     getCaption() {
         return this.caption;
@@ -68,8 +78,14 @@ class PostDto {
     setImageUrl(image_url) {
         this.image_url = image_url;
     }
+    setFiles(files) {
+        this.files = files;
+    }
     setUserId(user_id) {
         this.user_id = user_id;
+    }
+    setUserUuid(user_uuid) {
+        this.user_uuid = user_uuid;
     }
     setCaption(caption) {
         this.caption = caption;
@@ -83,28 +99,42 @@ class PostDto {
 }
 __decorate([
     (0, class_transformer_1.Exclude)({ toPlainOnly: true }),
+    (0, class_validator_1.ValidateIf)((o) => o.files.length === 0),
     __metadata("design:type", Number)
 ], PostDto.prototype, "id", void 0);
 __decorate([
     (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.ValidateIf)((o) => o.files.length === 0),
     (0, class_validator_1.IsNotEmpty)({ message: "UUID is required" }),
     (0, class_validator_1.IsUUID)(4, { message: "invalid search's UUID version" }),
     __metadata("design:type", Object)
 ], PostDto.prototype, "uuid", void 0);
 __decorate([
     (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.ValidateIf)((o) => o.files.length === 0),
     (0, class_validator_1.IsNotEmpty)({ message: "image ID is required" }),
     __metadata("design:type", String)
 ], PostDto.prototype, "image_id", void 0);
 __decorate([
     (0, class_transformer_1.Expose)(),
+    (0, class_validator_1.ValidateIf)((o) => o.files.length === 0),
     (0, class_validator_1.IsNotEmpty)({ message: "image URL is required" }),
     __metadata("design:type", Object)
 ], PostDto.prototype, "image_url", void 0);
 __decorate([
     (0, class_transformer_1.Exclude)({ toPlainOnly: true }),
+    (0, class_validator_1.ValidateIf)((o) => !o.id || !o.uuid || !o.image_id || !o.image_url),
+    (0, class_validator_1.ArrayNotEmpty)({ message: "files are required" }),
+    __metadata("design:type", Array)
+], PostDto.prototype, "files", void 0);
+__decorate([
+    (0, class_transformer_1.Exclude)({ toPlainOnly: true }),
     __metadata("design:type", Number)
 ], PostDto.prototype, "user_id", void 0);
+__decorate([
+    (0, class_transformer_1.Expose)(),
+    __metadata("design:type", Object)
+], PostDto.prototype, "user_uuid", void 0);
 __decorate([
     (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
--- server/src/domain/dto/post.dto.ts
+++ server/src/domain/dto/post.dto.ts
@@ -1,25 +1,37 @@
 import { Exclude, Expose }    from "class-transformer";
-import { IsNotEmpty, IsUUID } from "class-validator";
+import { ArrayNotEmpty, IsNotEmpty, IsUUID, ValidateIf } from "class-validator";
 
 class PostDto {
   @Exclude({ toPlainOnly: true })
+  @ValidateIf((o) => o.files.length === 0)
   private id: number;
 
   @Expose()
+  @ValidateIf((o) => o.files.length === 0)
   @IsNotEmpty({ message: "UUID is required" })
   @IsUUID(4, { message: "invalid search's UUID version" })
   private uuid: any;
 
   @Expose()
+  @ValidateIf((o) => o.files.length === 0)
   @IsNotEmpty({ message: "image ID is required" })
   private image_id: string;
 
   @Expose()
+  @ValidateIf((o) => o.files.length === 0)
   @IsNotEmpty({ message: "image URL is required" })
   private image_url: string | null;
 
   @Exclude({ toPlainOnly: true })
+  @ValidateIf((o) => !o.id || !o.uuid || !o.image_id || !o.image_url)
+  @ArrayNotEmpty({ message: "files are required" })
+  private files: Express.Multer.File[];
+
+  @Exclude({ toPlainOnly: true })
   private user_id: number;
+
+  @Expose()
+  private user_uuid: any;
 
   @Expose()
   private caption: string | null;
@@ -35,7 +47,9 @@ class PostDto {
     uuid: any,
     image_id: string,
     image_url: string | null,
+    files: Express.Multer.File[],
     user_id: number,
+    user_uuid: any,
     caption: string | null,
     privacy_level: string | null,
     created_at: Date | null
@@ -44,7 +58,9 @@ class PostDto {
     this.uuid = uuid;
     this.image_id = image_id;
     this.image_url = image_url;
+    this.files = files;
     this.user_id = user_id;
+    this.user_uuid = user_uuid;
     this.caption = caption;
     this.privacy_level = privacy_level;
     this.created_at = created_at;
@@ -67,8 +83,16 @@ class PostDto {
     return this.image_url;
   }
 
+  public getFiles(): Express.Multer.File[] {
+    return this.files;
+  }
+
   public getUserId(): number {
     return this.user_id;
+  }
+
+  public getUserUuid(): any {
+    return this.user_uuid;
   }
 
   public getCaption(): string | null {
@@ -100,8 +124,16 @@ class PostDto {
     this.image_url = image_url;
   }
 
+  public setFiles(files: Express.Multer.File[]): void {
+    this.files = files;
+  }
+
   public setUserId(user_id: number): void {
     this.user_id = user_id;
+  }
+
+  public setUserUuid(user_uuid: any): void {
+    this.user_uuid = user_uuid;
   }
 
   public setCaption(caption: string | null): void {
