f00f43ee1c38c63673d4d72faae11735bbfa86a4
darylescode <daryledevs@gmail.com>
2024-08-29 15:22:46 +0800

    Refactor auth router to use express-validator for request validation

--- server/dist/presentation/routers/auth.router.js
+++ server/dist/presentation/routers/auth.router.js
@@ -4,15 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const user_dto_1 = __importDefault(require("@/domain/dto/user.dto"));
 const auth_service_impl_1 = __importDefault(require("@/application/services/auth/auth.service.impl"));
 const auth_controller_1 = __importDefault(require("@/presentation/controllers/auth.controller"));
 const user_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/user.repository.impl"));
 const auth_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/auth.repository.impl"));
+const validate_request_data_validation_1 = __importDefault(require("../validations/validate-request-data.validation"));
+const validate_login_credentials_validation_1 = __importDefault(require("../validations/validate-login-credentials.validation"));
+const validate_empty_request_body_fields_1 = __importDefault(require("../validations/validate-empty-request-body-fields"));
+const express_validator_1 = require("express-validator");
 const router = express_1.default.Router();
 const controller = new auth_controller_1.default(new auth_service_impl_1.default(new auth_repository_impl_1.default(), new user_repository_impl_1.default()));
-router.post("/register", controller.register);
-router.post("/login", controller.login);
-router.post("/forgot-password", controller.forgotPassword);
+router
+    .route("/register")
+    .all((0, validate_empty_request_body_fields_1.default)(["id", "uuid"]), (0, validate_request_data_validation_1.default)(user_dto_1.default))
+    .post(controller.register);
+router
+    .route("/login")
+    .all(validate_login_credentials_validation_1.default)
+    .post(controller.login);
+router
+    .route("/forgot-password")
+    .all((0, express_validator_1.body)("email")
+    .exists()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("email not valid"))
+    .post(controller.forgotPassword);
 router.get("/reset-password-form", controller.resetPasswordForm);
 router.post("/reset-password", controller.resetPassword);
 router.get("/logout", controller.logout);
--- server/src/presentation/routers/auth.router.ts
+++ server/src/presentation/routers/auth.router.ts
@@ -1,8 +1,13 @@
-import express        from "express";
-import AuthService    from "@/application/services/auth/auth.service.impl";
-import AuthController from "@/presentation/controllers/auth.controller";
-import UserRepository from "@/infrastructure/repositories/user.repository.impl";
-import AuthRepository from "@/infrastructure/repositories/auth.repository.impl";
+import express                        from "express";
+import UserDto                        from "@/domain/dto/user.dto";
+import AuthService                    from "@/application/services/auth/auth.service.impl";
+import AuthController                 from "@/presentation/controllers/auth.controller";
+import UserRepository                 from "@/infrastructure/repositories/user.repository.impl";
+import AuthRepository                 from "@/infrastructure/repositories/auth.repository.impl";
+import validateRequestData            from "../validations/validate-request-data.validation";
+import validateLoginCredentials       from "../validations/validate-login-credentials.validation";
+import validateEmptyRequestBodyFields from "../validations/validate-empty-request-body-fields";
+import { body } from "express-validator";
 
 const router = express.Router();
 
@@ -13,9 +18,30 @@ const controller: AuthController = new AuthController(
   )
 );
 
-router.post("/register",           controller.register);
-router.post("/login",              controller.login);
-router.post("/forgot-password",    controller.forgotPassword);
+router
+  .route("/register")
+  .all(
+    validateEmptyRequestBodyFields(["id", "uuid"]),
+    validateRequestData(UserDto)
+  )
+  .post(controller.register);
+
+router
+  .route("/login")
+  .all(validateLoginCredentials)
+  .post(controller.login);
+
+router
+  .route("/forgot-password")
+  .all(
+    body("email")
+      .exists()
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email not valid")
+  )
+  .post(controller.forgotPassword);
+
 router.get("/reset-password-form", controller.resetPasswordForm);
 router.post("/reset-password",     controller.resetPassword);
 router.get("/logout",              controller.logout);
