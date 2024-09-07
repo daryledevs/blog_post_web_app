b66fdd2be87a9b38c863d775eedd158cd94076df
darylescode <daryledevs@gmail.com>
2024-09-07 16:14:58 +0800

    Refactor feed.router to use AsyncWrapper for error handling

--- server/dist/presentation/routers/feed.router.js
+++ server/dist/presentation/routers/feed.router.js
@@ -5,12 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const feed_service_impl_1 = __importDefault(require("@/application/services/feed/feed.service.impl"));
+const async_wrapper_util_1 = __importDefault(require("@/application/utils/async-wrapper.util"));
 const feed_controller_1 = __importDefault(require("@/presentation/controllers/feed.controller"));
 const feed_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/feed.repository.impl"));
 const user_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/user.repository.impl"));
 const router = express_1.default.Router();
+const wrap = new async_wrapper_util_1.default();
 const controller = new feed_controller_1.default(new feed_service_impl_1.default(new feed_repository_impl_1.default(), new user_repository_impl_1.default()));
-router.get("/count", controller.getTotalFeed);
-router.get("/explore", controller.getExploreFeed);
-router.post("/", controller.getUserFeed);
+router
+    .route("/count")
+    .get(wrap.asyncErrorHandler(controller.getTotalFeed));
+router
+    .route("/explore")
+    .get(wrap.asyncErrorHandler(controller.getExploreFeed));
+router
+    .route("/")
+    .post(wrap.asyncErrorHandler(controller.getUserFeed));
 exports.default = router;
--- server/src/presentation/routers/feed.router.ts
+++ server/src/presentation/routers/feed.router.ts
@@ -1,10 +1,12 @@
 import express        from "express";
 import FeedService    from "@/application/services/feed/feed.service.impl";
+import AsyncWrapper   from "@/application/utils/async-wrapper.util";
 import FeedController from "@/presentation/controllers/feed.controller";
 import FeedRepository from "@/infrastructure/repositories/feed.repository.impl";
 import UserRepository from "@/infrastructure/repositories/user.repository.impl";
 
 const router = express.Router();
+const wrap = new AsyncWrapper();
 
 const controller: FeedController = new FeedController(
   new FeedService(
@@ -13,8 +15,16 @@ const controller: FeedController = new FeedController(
   )
 );
 
-router.get("/count",   controller.getTotalFeed);
-router.get("/explore", controller.getExploreFeed);
-router.post("/",       controller.getUserFeed);
+router
+  .route("/count")
+  .get(wrap.asyncErrorHandler(controller.getTotalFeed));
+
+router
+  .route("/explore")
+  .get(wrap.asyncErrorHandler(controller.getExploreFeed));
+
+router
+  .route("/")
+  .post(wrap.asyncErrorHandler(controller.getUserFeed));
 
 export default router;
