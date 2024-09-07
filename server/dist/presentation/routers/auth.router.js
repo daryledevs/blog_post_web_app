83ac7bd1795f220eaeb026f0bdb8455d6b499a47
darylescode <daryledevs@gmail.com>
2024-09-07 16:56:40 +0800

    Refactor auth.router to use AsyncWrapper for error handling

--- server/dist/presentation/routers/auth.router.js
+++ server/dist/presentation/routers/auth.router.js
@@ -5,24 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_dto_1 = __importDefault(require("@/domain/dto/user.dto"));
+const express_validator_1 = require("express-validator");
 const auth_service_impl_1 = __importDefault(require("@/application/services/auth/auth.service.impl"));
+const async_wrapper_util_1 = __importDefault(require("@/application/utils/async-wrapper.util"));
 const auth_controller_1 = __importDefault(require("@/presentation/controllers/auth.controller"));
 const user_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/user.repository.impl"));
 const auth_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/auth.repository.impl"));
 const validate_request_data_validation_1 = __importDefault(require("../validations/validate-request-data.validation"));
 const validate_login_credentials_validation_1 = __importDefault(require("../validations/validate-login-credentials.validation"));
 const validate_empty_request_body_fields_1 = __importDefault(require("../validations/validate-empty-request-body-fields"));
-const express_validator_1 = require("express-validator");
 const router = express_1.default.Router();
+const wrap = new async_wrapper_util_1.default();
 const controller = new auth_controller_1.default(new auth_service_impl_1.default(new auth_repository_impl_1.default(), new user_repository_impl_1.default()));
 router
     .route("/register")
     .all((0, validate_empty_request_body_fields_1.default)(["id", "uuid"]), (0, validate_request_data_validation_1.default)(user_dto_1.default))
-    .post(controller.register);
+    .post(wrap.asyncErrorHandler(controller.register));
 router
     .route("/login")
     .all(validate_login_credentials_validation_1.default)
-    .post(controller.login);
+    .post(wrap.asyncErrorHandler(controller.login));
 router
     .route("/forgot-password")
     .all((0, express_validator_1.body)("email")
@@ -30,8 +32,16 @@ router
     .withMessage("email is required")
     .isEmail()
     .withMessage("email not valid"))
-    .post(controller.forgotPassword);
-router.get("/reset-password-form", controller.resetPasswordForm);
-router.post("/reset-password", controller.resetPassword);
-router.get("/logout", controller.logout);
+    .post(wrap.asyncErrorHandler(controller.forgotPassword));
+router
+    .route("/reset-password-form")
+    .all((0, validate_empty_request_body_fields_1.default)("token"))
+    .get(wrap.asyncErrorHandler(controller.resetPasswordForm));
+router
+    .route("/reset-password")
+    .all((0, validate_empty_request_body_fields_1.default)(["referenceToken"]), (0, validate_request_data_validation_1.default)(user_dto_1.default))
+    .post(wrap.asyncErrorHandler(controller.resetPassword));
+router
+    .route("/logout")
+    .get(wrap.asyncErrorHandler(controller.logout));
 exports.default = router;
--- server/src/presentation/routers/auth.router.ts
+++ server/src/presentation/routers/auth.router.ts
@@ -1,15 +1,17 @@
 import express                        from "express";
 import UserDto                        from "@/domain/dto/user.dto";
+import { body }                       from "express-validator";
 import AuthService                    from "@/application/services/auth/auth.service.impl";
+import AsyncWrapper                   from "@/application/utils/async-wrapper.util";
 import AuthController                 from "@/presentation/controllers/auth.controller";
 import UserRepository                 from "@/infrastructure/repositories/user.repository.impl";
 import AuthRepository                 from "@/infrastructure/repositories/auth.repository.impl";
 import validateRequestData            from "../validations/validate-request-data.validation";
 import validateLoginCredentials       from "../validations/validate-login-credentials.validation";
 import validateEmptyRequestBodyFields from "../validations/validate-empty-request-body-fields";
-import { body } from "express-validator";
 
 const router = express.Router();
+const wrap = new AsyncWrapper();
 
 const controller: AuthController = new AuthController(
   new AuthService(
@@ -24,12 +26,12 @@ router
     validateEmptyRequestBodyFields(["id", "uuid"]),
     validateRequestData(UserDto)
   )
-  .post(controller.register);
+  .post(wrap.asyncErrorHandler(controller.register));
 
 router
   .route("/login")
   .all(validateLoginCredentials)
-  .post(controller.login);
+  .post(wrap.asyncErrorHandler(controller.login));
 
 router
   .route("/forgot-password")
@@ -40,10 +42,24 @@ router
       .isEmail()
       .withMessage("email not valid")
   )
-  .post(controller.forgotPassword);
+  .post(wrap.asyncErrorHandler(controller.forgotPassword));
 
-router.get("/reset-password-form", controller.resetPasswordForm);
-router.post("/reset-password",     controller.resetPassword);
-router.get("/logout",              controller.logout);
+router
+  .route("/reset-password-form")
+  .all(validateEmptyRequestBodyFields("token"))
+  .get(wrap.asyncErrorHandler(controller.resetPasswordForm));
+
+router
+  .route("/reset-password")
+  .all(
+    validateEmptyRequestBodyFields(["referenceToken"]),
+    validateRequestData(UserDto),
+
+  )
+  .post(wrap.asyncErrorHandler(controller.resetPassword));
+
+router
+  .route("/logout")
+  .get(wrap.asyncErrorHandler(controller.logout));
 
 export default router;
