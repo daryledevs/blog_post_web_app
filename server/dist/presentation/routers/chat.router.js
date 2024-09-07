f093eb7c45a0889c1a461b0f986ae9a0b1d03eae
darylescode <daryledevs@gmail.com>
2024-09-07 16:06:12 +0800

    Refactor chat.router.ts to use asyncErrorHandler for error handling

--- server/dist/presentation/routers/chat.router.js
+++ server/dist/presentation/routers/chat.router.js
@@ -4,14 +4,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const async_wrapper_util_1 = __importDefault(require("@/application/utils/async-wrapper.util"));
 const chat_service_impl_1 = __importDefault(require("@/application/services/chat/chat.service.impl"));
 const user_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/user.repository.impl"));
 const chat_controller_1 = __importDefault(require("@/presentation/controllers/chat.controller"));
 const chat_repository_impl_1 = __importDefault(require("@/infrastructure/repositories/chat.repository.impl"));
+const validate_uuid_body_validation_1 = __importDefault(require("../validations/validate-uuid-body.validation"));
+const validate_uuid_params_validation_1 = __importDefault(require("../validations/validate-uuid-params.validation"));
 const router = express_1.default.Router();
+const wrap = new async_wrapper_util_1.default();
 const controller = new chat_controller_1.default(new chat_service_impl_1.default(new chat_repository_impl_1.default(), new user_repository_impl_1.default()));
-router.post("/:uuid/messages", controller.getChatMessages);
-router.post("/lists", controller.getChatHistory);
-router.post("/", controller.newMessageAndConversation);
-router.delete("/:uuid", controller.deleteConversationById);
+router
+    .route("/:uuid")
+    .all((0, validate_uuid_params_validation_1.default)("uuid"), (0, validate_uuid_body_validation_1.default)("messageIds"))
+    .post(wrap.asyncErrorHandler(controller.getChatMessages))
+    .delete(wrap.asyncErrorHandler(controller.deleteConversationById));
+router
+    .route("/chat-history")
+    .all((0, validate_uuid_params_validation_1.default)("user_uuid"), (0, validate_uuid_body_validation_1.default)("conversationIds"))
+    .post(wrap.asyncErrorHandler(controller.getChatHistory));
+router
+    .route("/")
+    .all((0, validate_uuid_body_validation_1.default)([
+    "conversation_uuid",
+    "sender_uuid",
+    "receiver_uuid"
+]))
+    .post(wrap.asyncErrorHandler(controller.newMessageAndConversation));
 exports.default = router;
--- server/src/presentation/routers/chat.router.ts
+++ server/src/presentation/routers/chat.router.ts
@@ -1,11 +1,15 @@
 
-import express         from "express";
-import ChatsServices   from "@/application/services/chat/chat.service.impl";
-import UserRepository  from "@/infrastructure/repositories/user.repository.impl";
-import ChatsController from "@/presentation/controllers/chat.controller";
-import ChatsRepository from "@/infrastructure/repositories/chat.repository.impl";
+import express                   from "express";
+import AsyncWrapper              from "@/application/utils/async-wrapper.util";
+import ChatsServices             from "@/application/services/chat/chat.service.impl";
+import UserRepository            from "@/infrastructure/repositories/user.repository.impl";
+import ChatsController           from "@/presentation/controllers/chat.controller";
+import ChatsRepository           from "@/infrastructure/repositories/chat.repository.impl";
+import validateUUIDRequestBody   from "../validations/validate-uuid-body.validation";
+import validateUUIDRequestParams from "../validations/validate-uuid-params.validation";
 
 const router = express.Router();
+const wrap = new AsyncWrapper();
 
 const controller: ChatsController = new ChatsController(
   new ChatsServices(
@@ -14,9 +18,30 @@ const controller: ChatsController = new ChatsController(
   )
 );
 
-router.post("/:uuid/messages",  controller.getChatMessages);
-router.post("/lists",           controller.getChatHistory);
-router.post("/",                controller.newMessageAndConversation);
-router.delete("/:uuid",         controller.deleteConversationById)
+router
+  .route("/:uuid")
+  .all(
+    validateUUIDRequestParams("uuid"), 
+    validateUUIDRequestBody("messageIds")
+  )
+  .post(wrap.asyncErrorHandler(controller.getChatMessages))
+  .delete(wrap.asyncErrorHandler(controller.deleteConversationById));
+  
+router
+  .route("/chat-history")
+  .all(
+    validateUUIDRequestParams("user_uuid"),
+    validateUUIDRequestBody("conversationIds")
+  )
+  .post(wrap.asyncErrorHandler(controller.getChatHistory));
+
+router
+  .route("/")
+  .all(validateUUIDRequestBody([
+    "conversation_uuid", 
+    "sender_uuid", 
+    "receiver_uuid"
+  ]))
+  .post(wrap.asyncErrorHandler(controller.newMessageAndConversation));
 
 export default router;
