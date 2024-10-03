621cd7454456b91c3fa63b5d830a746dbdbbae26
darylescode <daryledevs@gmail.com>
2024-10-04 01:01:19 +0800

    Refactor IChat interface to include textMessage and timeSent properties

--- server/src/domain/types/chat.interface.ts
+++ server/src/domain/types/chat.interface.ts
@@ -1,10 +1,12 @@
 import { IUserDetails } from "./user.interface";
 
-export interface IChat  extends Omit<IUserDetails, "age" | "birthday">{
+export interface IChat extends Omit<IUserDetails, "age" | "birthday"> {
   id: number;
   uuid: any;
   conversationId: number;
   conversationUuid: any;
   userUuid: any;
   username: string;
+  textMessage: string | null;
+  timeSent: Date;
 }
